39ab271aae942144afa0809ca7cbb58ee51f7d4b
unknown <chigrus@yandex.ru>
2023-05-07 14:54:02 +0300

    modified authorization

--- src/routes/api/authorization/+server.ts
+++ src/routes/api/authorization/+server.ts
@@ -21,10 +21,10 @@ export const POST:Action = async ({ request, locals, cookies }) => {
       throw error(403, `Пользователь с логином ${login} и указанным паролем не обнаружен!`);
     }
 
-    // const token = await jwtSign({
-    //     id: userData[0].id,
-    //     role: userData[0].user_role
-    // });
+    const token = await jwtSign({
+        id: userData[0].id,
+        role: userData[0].user_role
+    });
 
     // cookies.set('token', token, {
     //   maxAge: 60 * 60 * 24 * 1,  //set cookie to expire after a one day
@@ -33,5 +33,5 @@ export const POST:Action = async ({ request, locals, cookies }) => {
     // });
 
   
-    return json(userData);
+    return json(token);
   }
