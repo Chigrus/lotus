a5c673f5b0cbfcb05ed2da27abd6e679eb039095
unknown <chigrus@yandex.ru>
2023-11-20 13:34:44 +0300

    modify for notes google speed

--- src/routes/about/+page.server.ts
+++ src/routes/about/+page.server.ts
@@ -5,7 +5,7 @@ import * as token from '$lib/server/token';
 let og_id = 2;
 let post_id = 15;
  
-export const load:Action= async ({ locals, cookies, setHeaders }) => {
+export const load:Action= async ({ locals, cookies }) => {
   let og_data = await db.getOG(locals, og_id);
   let post = await db.getPost(locals, post_id);
   let user = await token.getUser(cookies);
@@ -19,9 +19,9 @@ export const load:Action= async ({ locals, cookies, setHeaders }) => {
     throw error(404);
   }
 
-  setHeaders({
-		'cache-control': 'max-age=31536000',
-	});
+  // setHeaders({
+	// 	'cache-control': 'max-age=31536000',
+	// });
 
   return {
     og_data, post, user,
